89a0a1d25109dfb2bd80630b0a0c072334cec64a
Sinantosun <sinan_tosun4141@hotmail.com>
2025-08-26 16:53:28 +0300

    Test Dev Branch Commit

--- src/Tools/projectenviorment.ts
+++ src/Tools/projectenviorment.ts
@@ -1,4 +1,5 @@
 export const ApiDefaults = {
-    apiUrl: "https://testprojectwebapi.sinantosun.com/api/"
+    //apiUrl: "https://testprojectwebapi.sinantosun.com/api/"
     //apiUrl: "https://localhost:7061/api/"
+    apiUrl: "Test Api url"
 }
